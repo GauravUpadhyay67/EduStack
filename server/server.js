53f8c3c7dab7b0e5acb73a123c5810353c97fb0a
Gaurav Upadhyay <upadhyaykumar2003@gmail.com>
2025-05-06 19:05:49 +0530

    added vercel.json and created user model

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ import express from 'express';
 import cors from 'cors';
 import connectDB from './configs/mongodb.js';
 import dotenv from 'dotenv';
+import { clerkWebhooks } from './controllers/webhooks.js';
 
 dotenv.config()
 
@@ -18,6 +19,8 @@ await connectDB()
 app.get('/', (req, res) => {
   res.send('Hello India!');
 });
+
+app.post('/clerk', express.json(), clerkWebhooks);
 
 // Port
 app.listen(port, () => {
