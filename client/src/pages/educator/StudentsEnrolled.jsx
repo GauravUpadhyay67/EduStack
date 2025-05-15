d4acb183b5079ec931b656bc0d39d943e9b35fae
Gaurav Upadhyay <upadhyaykumar2003@gmail.com>
2025-05-15 22:56:40 +0530

    Finalize project implementation — open to future improvements and optimizations

--- client/src/pages/educator/StudentsEnrolled.jsx
+++ client/src/pages/educator/StudentsEnrolled.jsx
@@ -1,18 +1,38 @@
-import React, { useEffect, useState } from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 import { dummyStudentEnrolled } from '../../assets/assets'
 import Loading from '../../components/student/Loading';
+import { AppContext } from '../../context/AppContext';
+import { toast } from 'react-toastify';
+import axios from 'axios';
 
 const StudentsEnrolled = () => {
 
+  const { backendUrl, getToken, isEducator } = useContext(AppContext)
   const [enrolledStudents, setEnrolledStudents] = useState(null);
 
   const fetchEnrolledStudents = async () => {
-    setEnrolledStudents(dummyStudentEnrolled)
+    try {
+      const token = await getToken();
+      const { data } = await axios.get(backendUrl + '/api/educator/enrolled-students', 
+        {headers: { Authorization: `Bearer ${token}` }}
+      )
+
+      if(data.success){
+        setEnrolledStudents(data.enrolledStudents.reverse())
+      } else {
+        toast.error(data.message)
+      }
+      
+    } catch (error) {
+      toast.error(error.message)
+    }
   }
 
   useEffect(() => {
-    fetchEnrolledStudents()
-  }, [])
+    if(isEducator){
+      fetchEnrolledStudents()
+    }
+  }, [isEducator])
 
   return enrolledStudents ? (
     <div className='min-h-screen flex flex-col items-start justify-between md:p-8 md:pb-0 p-4 pt-8 pb-0'>
@@ -28,6 +48,7 @@ const StudentsEnrolled = () => {
         </thead>
         <tbody className='text-sm text-gray-500'>
           {enrolledStudents.map((item, index) => (
+            
             <tr key={index} className='border-b border-gray-500/20'>
               <td className='px-4 py-3 text-center hidden sm:table-cell'>{index + 1}</td>
               <td className='md:px-4 px-2 py-3 flex items-center space-x-3'>
@@ -35,7 +56,7 @@ const StudentsEnrolled = () => {
                 <span className='truncate'>{item.student.name}</span>
               </td>
               <td className='px-4 py-3 truncate'>{item.courseTitle}</td>
-              <td className='px-4 py-3 hidden sm:table-cell'>{new Date(item.purchaseDate).toLocaleDateString()}</td>
+              <td className='px-4 py-3 hidden sm:table-cell'>{item.purchaseDate ? new Date(item.purchaseDate).toLocaleDateString() : "N/A"}</td>
             </tr>
           ))}
         </tbody>
--- client/src/pages/student/CourseDetails.jsx
+++ client/src/pages/student/CourseDetails.jsx
@@ -6,6 +6,8 @@ import { assets } from "../../assets/assets";
 import humanizeDuration from "humanize-duration";
 import Footer from "../../components/student/Footer";
 import YouTube from "react-youtube";
+import axios from "axios";
+import { toast } from "react-toastify";
 
 const CourseDetails = () => {
   const { id } = useParams();
@@ -14,16 +16,57 @@ const CourseDetails = () => {
   const [isAlreadyEnrolled, setIsAlreadyEnrolled] = useState(false);
   const [playerData, setPlayerData] = useState(null);
 
-  const { allCourses, calculateRating, calculateChapterTime, calculateCourseDuration, calculateNoOfLectures, currency } = useContext(AppContext);
+  const { allCourses, calculateRating, calculateChapterTime, calculateCourseDuration, calculateNoOfLectures, currency, backendUrl, userData, getToken } = useContext(AppContext);
 
   const fetchCourseData = async () => {
-    const findCourse = allCourses.find((course) => course._id === id);
-    setCourseData(findCourse);
+    try {
+      const { data } = await axios.get(backendUrl + '/api/course/' + id)
+
+      if(data.success){
+        setCourseData(data.courseData)
+      } else {
+        toast.error(data.message)
+      }
+      
+    } catch (error) {
+      toast.error(error.message)
+    }
   };
+
+  const enrollCourse = async () => {
+    try {
+      if(!userData){
+        return toast.warn('Login to Enroll')
+      }
+
+      if(isAlreadyEnrolled){
+        return toast.warn('Already Enrolled')
+      }
+
+      const token = await getToken();
+      const { data } = await axios.post(backendUrl + '/api/user/purchase', { courseId: courseData._id }, { headers: { Authorization: `Bearer ${token}` }})
+
+      if(data.success){
+        const { session_url } = data;
+        window.location.replace(session_url);
+      } else {
+        toast.error(data.message)
+      }
+      
+    } catch (error) {
+      toast.error(error.message)
+    }
+  }
 
   useEffect(() => {
     fetchCourseData();
-  }, [allCourses]);
+  }, []);
+
+  useEffect(() => {
+    if(userData && courseData){
+      setIsAlreadyEnrolled(userData.enrolledCourses.includes(courseData._id))
+    }
+  }, [userData, courseData]);
 
   const toggleSection = (index) => {
     setOpenSections((prev) => (
@@ -71,7 +114,7 @@ const CourseDetails = () => {
             <p className="text-gray-500">{courseData.enrolledStudents.length} {courseData.enrolledStudents.length > 1 ? 'students' : 'student'}</p>
           </div>
 
-          <p className="text-sm">Course by <span className="text-blue-600 underline">Striver</span></p>
+          <p className="text-sm">Course by <span className="text-blue-600 underline">{courseData.educator.name}</span></p>
 
           <div className="pt-8 text-gray-800">
             <h2 className="text-xl font-semibold">Course Structure</h2>
@@ -175,7 +218,7 @@ const CourseDetails = () => {
 
             </div>
 
-            <button className="md:my-6 mt-4 w-full py-3 rounded bg-blue-600 text-white font-medium">
+            <button onClick={enrollCourse} className="md:my-6 mt-4 w-full py-3 rounded bg-blue-600 text-white font-medium">
               {isAlreadyEnrolled ? 'Already Enrolled' : 'Enrolled Now'}
             </button>
 
